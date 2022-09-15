a8571e9021e77f9c6c98b7671db227deefe83661
MatheusBruno <matheusbrunosilveira@gmail.com>
2022-09-14 22:10:43 -0300

    .

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -1,9 +1,15 @@
 import { Link } from 'react-router-dom';
 import style from './NavBar.module.css';
+import logo from '../imgs/costs_logo.png';
 
 function NavBar(){
     return(
         <nav className={style.main}>
+
+            <Link to="/" className={style.linkimg}>
+                <img src={logo} className={style.logo}></img>
+            </Link>
+
             <ul className={style.alinhamento}>
                 <Link to="/" className={style.links}>Home</Link>
                 <Link to="/empresa" className={style.links}>Empresa</Link>
